cdeda551fe64bc695aa0f956f1f6765e0b0395bf
Coki_Zhao <coki_zhao@epam.com>
2025-01-08 17:24:50 +0800

    android show pic bug fix

--- main.py
+++ main.py
@@ -1,38 +1,85 @@
+from kivy.core.window import Window
 from kivy.lang import Builder
+
 from kivymd.app import MDApp
-from plyer import filechooser
+from kivymd.uix.filemanager import MDFileManager
+from kivymd.toast import toast
+from kivy.utils import platform
+import os
+
 
 KV = '''
-MDScreen:
-    BoxLayout:
-        orientation: "vertical"
-        pos_hint: {"center_x": .5, "center_y": .5}
-        size_hint: .4, .7
+BoxLayout:
+    orientation: 'vertical'
 
-        MDRectangleFlatButton:
-            on_release: app.choose()
-            pos_hint: {"center_x": .5}
-            text: "Select a file"
+
+    FloatLayout:
+
+        MDRoundFlatIconButton:
+            text: "Open manager"
+            icon: "folder"
+            pos_hint: {'center_x': .5, 'center_y': .1}
+            on_release: app.file_manager_open()
+            
+
         FitImage:
+            pos_hint: {'center_x': .5, 'center_y': .3}
+            size_hint: 0.5, 0.3
             id: image_show
-            size_hint_y: 1
-            pos_hint: {"top": 1}
+        FitImage:
+            pos_hint: {'center_x': .5, 'center_y': .6}
+            size_hint: 0.5, 0.3
+            source: '1.png'
 '''
 
-class MainApp(MDApp):
+
+class Example(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        Window.bind(on_keyboard=self.events)
+        self.manager_open = False
+        self.file_manager = MDFileManager(
+            exit_manager=self.exit_manager,
+            select_path=self.select_path,
+            preview=True
+        )
+
     def build(self):
-        self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Orange"
         return Builder.load_string(KV)
 
-    def choose(self):
-        # 打开文件选择器
-        filepath = filechooser.open_file(title="Pick a file..", filters=[("All Files", "*.*")])
-        # 更新标签显示所选文件路径
-        if filepath:
-            path = filepath[0]
-            if path.endswith(".png") or path.endswith(".jpg") or path.endswith(".jpeg"):
-                self.root.ids.image_show.source = path
+    def file_manager_open(self):
+        self.file_manager.show_disks()
+        PATH ="."
+        if platform == "android":
+          PATH = "/storage/emulated/0/Pictures" #app_folder
+        self.file_manager.show(PATH)  # output manager to the screen
+        self.manager_open = True
+
+    def select_path(self, path):
+        '''It will be called when you click on the file name
+        or the catalog selection button.
+
+        :type path: str;
+        :param path: path to the selected directory or file;
+        '''
+
+        self.exit_manager()
+        toast(path)
+        self.root.ids.image_show.source = path
+
+    def exit_manager(self, *args):
+        '''Called when the user reaches the root of the directory tree.'''
+
+        self.manager_open = False
+        self.file_manager.close()
+
+    def events(self, instance, keyboard, keycode, text, modifiers):
+        '''Called when buttons are pressed on the mobile device.'''
+
+        if keyboard in (1001, 27):
+            if self.manager_open:
+                self.file_manager.back()
+        return True
 
 
-MainApp().run()
+Example().run()
